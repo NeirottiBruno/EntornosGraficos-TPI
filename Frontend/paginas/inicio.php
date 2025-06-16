4e723b766809f1156363e2e237f095914090f5e3
Felipe <bentancourfelipe@gmail.com>
2025-06-16 18:12:34 -0300

    Agregar mapa en el inicio

--- Frontend/paginas/inicio.php
+++ Frontend/paginas/inicio.php
@@ -23,8 +23,8 @@ include '../componentes/encabezado.php';
 
 
     <section class="container-fluid margen-grande">
-        <h1 class="mb-4 text-center">Secciones que te pueden interesar</h1>
-        <div class="row g-5"> <!-- g-4 agrega más separación entre columnas y filas -->
+        <h1 class="mb-4 text-center">¡Secciones que te pueden interesar!</h1>
+        <div class="row g-5">
             <div class="col-12 col-md-6 d-flex">
                 <a href="locales.php" class="text-decoration-none flex-fill">
                     <div class="card text-bg-dark h-100">
@@ -48,12 +48,8 @@ include '../componentes/encabezado.php';
         </div>
     </section>
 
-    <!-- Agregá más categorías si querés -->
-
-
-    <!-- Promociones destacadas (ejemplo) -->
     <section class=" container-fluid margen-grande">
-        <h1 class="mb-4 text-center">Ultimas novedades</h1>
+        <h1 class="mb-4 text-center">¡Ultimas novedades!</h1>
         <div class="row row-cols-1 row-cols-3  g-4">
 
             <div class="col">
@@ -86,6 +82,19 @@ include '../componentes/encabezado.php';
 
         </div>
     </section>
+
+<div class="container-fluid my-5">
+    <h1 class="text-center mb-4">¡Encontranos!</h1>
+    <div style="width: 100%; max-width: 100%; height: 350px; overflow: hidden; ">
+        <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d26785.77062545234!2d-60.681763919906345!3d-32.94516549056678!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95b7ab10c3a2cb0f%3A0x5d19f654fdac9e11!2sGALERIA%20PLAZA%20SARMIENTO!5e0!3m2!1ses!2ar!4v1750107627898!5m2!1ses!2ar"
+            width="100%" height="100%" style="border:0;" 
+            allowfullscreen="" 
+            loading="lazy" 
+            referrerpolicy="no-referrer-when-downgrade">
+        </iframe>
+    </div>
+</div>
 
     <?php include '../componentes/pie.php'; ?>
 </body>
